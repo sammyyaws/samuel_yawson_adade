9956e6945bcc007d20b2224cb826ce43e4fa0b91
Samuel Yawson Adade <sammyyawsnana1@gmail.com>
2024-09-26 12:03:32 +0000

    overall goal

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # JAVASCRIPT
 90 days javascript learning journey 
 It is create pleasure to cpme back to coding again all this entire time of looking for a a career worth paying
+## I have 3 months to get myself a job in the it world.
---
+++ script.js
@@ -0,0 +1,27 @@
+alert("hello \' IT IS WHAT IT IS\'");
+/** arrays */
+
+const Items=['car',"houses",'books',1000,];
+console.log(Items.length);
+console.log(Items[0]);
+/**objects */
+const Schools={
+    Botwe:"red",
+    Augusco:"green",
+    Site:"purple",
+}; 
+
+console.log(Schools.Botwe);
+
+/** functions */
+
+function ConsolePlay(){
+    const Schools={
+        Botwe:"red",
+        Augusco:"green",
+        Site:"purple",
+    }; 
+
+    console.log(Schools.Site)
+}
+  ConsolePlay(); 
